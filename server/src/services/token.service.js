80eadd43e87711eacf0dc684bb577ffe1a4398d2
deyantomov <deyan.tomov.a58@learn.telerikacademy.com>
2024-09-20 17:27:14 +0300

    feat: token validation

--- server/src/services/token.service.js
+++ server/src/services/token.service.js
@@ -6,8 +6,8 @@ dotenv.config();
 const jwtSecretKey = process.env.JWT_SECRET || "";
 
 export function generateToken(username) {
-  const timeNow = Math.floor(Date.now() / 1000);  // must be in seconds since Unix epoch (1/1/1970)
-  
+  const timeNow = Math.floor(Date.now() / 1000); // must be in seconds since Unix epoch (1/1/1970)
+
   const data = {
     iss: "BankSmart", // issuer
     name: username, // holder
@@ -17,4 +17,13 @@ export function generateToken(username) {
 
   const token = jwt.sign(data, jwtSecretKey);
   return token;
+}
+
+export function validateToken(token) {
+  try {
+    const decoded = jwt.verify(token, jwtSecretKey);
+    return decoded;
+  } catch (err) {
+    throw new Error("Invalid token");
+  }
 }

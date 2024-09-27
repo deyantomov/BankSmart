b11aa1753a67c7f180fdaf633d226e34d82144f6
deyantomov <deyan.tomov.a58@learn.telerikacademy.com>
2024-09-27 18:44:50 +0300

    feat: add transaction history in transactions page

--- client/src/components/tiles/TransactionTile.tsx
+++ client/src/components/tiles/TransactionTile.tsx
@@ -1,21 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
-import { Link } from "react-router-dom";
 import { Card, Table, Button } from "react-daisyui";
 import { FiPlusCircle } from "react-icons/fi";
 
 export default function TransactionTile() {
+  const navigate = useNavigate();
   const { transactions } = useSelector(
     (state: RootState) => state.transactionData
   );
-
   const { userData: user } = useSelector((state: RootState) => state);
+
+  const handleViewDetails = () => {
+    navigate("/transactions");
+  }
 
   return (
     <div className="flex flex-col gap-4 w-full h-auto px-4 py-4">
       <div className="flex justify-between items-center w-full">
         <h1 className="p-4 text-3xl">Transactions</h1>
-        <Button className="btn btn-ghost text-primary text-xl rounded-md">
+        <Button 
+          className="btn btn-ghost text-primary text-xl rounded-md"
+          onClick={handleViewDetails}
+        >
           View details
         </Button>
       </div>
@@ -63,10 +70,13 @@ export default function TransactionTile() {
       ) : (
         <Card className="flex flex-col justify-center items-center w-full h-auto p-8 bg-slate-200 text-lg md:text-xl col-span-3">
           <h2 className="text-xl mb-6">No transactions</h2>
-          <Link to="/transactions" className="flex flex-col items-center">
+          <div 
+            className="flex flex-col items-center"
+            onClick={handleViewDetails}
+          >
             <FiPlusCircle size={32} className="mb-1 text-success" />
             <p className="text-xs text-gray">See more information here</p>
-          </Link>
+          </div>
         </Card>
       )}
     </div>
--- client/src/views/Transactions/Transactions.tsx
+++ client/src/views/Transactions/Transactions.tsx
@@ -1,24 +1,73 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
 import withNavigation from "../../hoc/withNavigation/withNavigation";
-import { Card } from "react-daisyui";
+import { Card, Button } from "react-daisyui";
 
 function Transactions() {
+  const { transactionData: { transactions } } = useSelector((state: RootState) => state);
+
+  console.log(transactions);
+
+  // Filter transactions based on type
+  const deposits = transactions.filter((transaction: any) => transaction.type === 'deposit');
+  const withdrawals = transactions.filter((transaction: any) => transaction.type === 'withdrawal');
+  const transfers = transactions.filter((transaction: any) => transaction.type === 'transfer');
+
   return (
     <div className="w-full h-full p-4 text-xl">
       <h1 className="p-4 text-3xl">New transactions</h1>
-      <div className="grid grid-cols-3 w-full h-2/3 p-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-3 w-full h-1/2 p-4 gap-8">
         <Card className="flex justify-center items-center bg-slate-200">
-          Deposit
+          <Button className="btn btn-ghost w-1/3 text-xl">Deposit</Button>
         </Card>
         <Card className="flex justify-center items-center bg-slate-200">
-          Withdraw
+          <Button className="btn btn-ghost w-1/3 text-xl">Withdraw</Button>
         </Card>
         <Card className="flex justify-center items-center bg-slate-200">
-          Transfer
+          <Button className="btn btn-ghost w-1/3 text-xl">Transfer</Button>
         </Card>
       </div>
       <h1 className="p-4 text-3xl">Transaction history</h1>
+      {transactions.length > 0 ? 
+      (
+        <div className="w-full h-1/3 p-4 text-xl">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 w-full h-full p-4">
+            <div>
+              <h2 className="text-2xl mb-4">Deposits</h2>
+              {deposits.map((transaction: any, index: number) => (
+                <Card key={index} className="text-xl bg-slate-200 p-4 mb-4">
+                  <p>{transaction.type}</p>
+                </Card>
+              ))}
+            </div>
+            <div>
+              <h2 className="text-2xl mb-4">Withdrawals</h2>
+              {withdrawals.map((transaction: any, index: number) => (
+                <Card key={index} className="text-xl bg-slate-200 p-4 mb-4">
+                  <p>{transaction.type}</p>
+                </Card>
+              ))}
+            </div>
+            <div>
+              <h2 className="text-2xl mb-4">Transfers</h2>
+              {transfers.map((transaction: any, index: number) => (
+                <Card key={index} className="text-xl bg-slate-200 p-4 mb-4">
+                  <p>{transaction.type}</p>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : 
+      (
+        <div className="w-full h-1/3 p-4 text-xl">
+          <div className="flex w-full h-full justify-center items-center p-4 bg-slate-200">
+            <h2>Your history is empty</h2>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default withNavigation(Transactions);
